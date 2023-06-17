675dcaf3b59d60972f386fba57877c256cda1e1c
Adam Jones <mail@ajones.co.uk>
2023-06-17 01:13:35 +0100

    Update to GCC 13.1

--- boot.sh
+++ boot.sh
@@ -1,8 +1,8 @@
-#! /bin/bash    
+#!/bin/bash
 qemu-system-x86_64 \
 -m 8G \
 -boot d \
 -smp 2 \
 -usb \
--drive if=pflash,format=raw,readonly,file=/usr/share/OVMF/OVMF_CODE.fd \
--drive file=$HOME/projects/caracal/images/caracal.iso,if=ide,index=1,media=cdrom
+-drive if=pflash,format=raw,readonly=on,file=/usr/share/OVMF/OVMF_CODE.fd \
+-drive file=$HOME/caracal/images/caracal.iso,if=ide,index=1,media=cdrom

e6cbb65aa079ac43dfbcabe3072f47fb6484291e
Adam Jones <admin@devonsoftware.co.uk>
2023-07-25 15:27:16 +0100

    Back to C++

--- libkernel/src/runtime/itoa.cpp
+++ libkernel/src/runtime/itoa.cpp
@@ -4,7 +4,7 @@
 const char ltHexUpper_[16] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 const char ltHexLower_[16] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
 
-char* itoa(uint64_t num, char* buffer, uint8_t base) -> string
+char* itoa(uint64_t num, char* buffer, uint8_t base)
 {
 	int index = 0;
 	int bitshift;
